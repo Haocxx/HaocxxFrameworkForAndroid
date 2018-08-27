b861befb7d6d342cdf0cd9b95511b39b1fe3dac4
haocxx <479477141@qq.com>
2018-08-27 16:59:52 +0800

    update NoScrollGridView

--- app/src/main/java/com/haocxx/haocxxframework/view/listview/NoScrollGridView.java
+++ app/src/main/java/com/haocxx/haocxxframework/view/listview/NoScrollGridView.java
@@ -19,7 +19,7 @@ public class NoScrollGridView extends GridView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,MeasureSpec.AT_MOST);
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
         super.onMeasure(widthMeasureSpec, expandSpec);
     }
 }
