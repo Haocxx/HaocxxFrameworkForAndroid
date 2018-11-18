f805808891a780a6224649812dc014ca6d8fb727
Haocxx <479477141@qq.com>
2018-11-18 11:40:35 +0800

    update

--- haocxxframework/src/main/java/com/haocxx/framework/haocxxobserver/HaocxxObserverManager.kt
+++ haocxxframework/src/main/java/com/haocxx/framework/haocxxobserver/HaocxxObserverManager.kt
@@ -3,6 +3,8 @@ package com.haocxx.framework.haocxxobserver
 import com.haocxx.framework.haocxxobserver.observer.BaseObserver
 
 /**
+ * The middleware manager for observe logic.
+ *
  * Created by Haocxx 
  * on 2018/11/18
  */
@@ -17,18 +19,28 @@ class HaocxxObserverManager {
 
     private val mObserverSet = HashSet<BaseObserver>()
 
+    /**
+     *  Register an observer in the observer set. It`s a thread safe method.
+     */
     fun regist(observer : BaseObserver) {
         synchronized(mObserverSet) {
             mObserverSet.add(observer)
         }
     }
 
+    /**
+     * Unregister an observer from the observer set. It`s a thread safe method.
+     * It should be invoked to prevent memory leak.
+     */
     fun unregist(observer : BaseObserver) {
         synchronized(mObserverSet) {
             mObserverSet.remove(observer)
         }
     }
 
+    /**
+     * Do notify to all the observers in the set. It`s a thread safe method.
+     */
     fun doNotify() {
         synchronized(mObserverSet) {
             for (observer in mObserverSet) {
--- haocxxframework/src/main/java/com/haocxx/framework/haocxxobserver/observer/BaseObserver.kt
+++ haocxxframework/src/main/java/com/haocxx/framework/haocxxobserver/observer/BaseObserver.kt
@@ -1,7 +1,7 @@
 package com.haocxx.framework.haocxxobserver.observer
 
 /**
- * Base interface for observer.
+ * Base interface for observers.
  *
  * Created by Haocxx 
  * on 2018/11/18
